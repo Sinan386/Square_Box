eb81085788d8bfec1a096fc8b15ef3ff443848ff
Sinan Demiroz <ton@email.com>
2025-11-13 11:07:13 +0100

    Initial commit

---
+++ src/main/java/com/example/squaregames/SquaregamesApplication.java
@@ -0,0 +1,13 @@
+package com.example.squaregames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquaregamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquaregamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/squaregames/SquaregamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.squaregames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquaregamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
