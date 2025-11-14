7e0ff32e30c84d29eb45da2d136cea92450be574
Sinan Demiroz <ton@email.com>
2025-11-14 16:57:30 +0100

    MAJ

--- src/main/java/com/example/squaregames/GameCatalogImpl.java
+++ src/main/java/com/example/squaregames/GameCatalogImpl.java
@@ -1,12 +1,19 @@
 package com.example.squaregames;
 
 import fr.le_campus_numerique.square_games.engine.tictactoe.TicTacToeGameFactory;
+import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.List;
+
+@Service
 public class GameCatalogImpl implements GameCatalog {
-    private final TicTacToeGameFactory tictactoeGameFactory = new TicTacToeGameFactory();
 
+    private final TicTacToeGameFactory ticTacToeFactory = new TicTacToeGameFactory();
 
+    @Override
+    public Collection<String> getGameIdentifiers() {
 
-
-
+        return List.of(ticTacToeFactory.getGameFactoryId());
+    }
 }
